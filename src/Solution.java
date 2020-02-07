ba820da04ef9f7bd6f97a9ec625253043939c01b
serrrgius <serrrgius@gmail.com>
2020-02-07 16:19:39 +0400

    Новый бранч для эксперимента 1

--- src/Solution.java
+++ src/Solution.java
@@ -4,7 +4,8 @@ public class Solution {
     String s = ""+aa;
 
     public static void main(String[] args) {
-        System.out.println(aa);
+
+        System.out.println("Замутили афигенную фичу");
     }
 
 }
